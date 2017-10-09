3ef6c045b124a51d22247f4e6fa31bef475887a1
meg <oobridgeoo@gmail.com>
2017-10-09 09:49:17 +0000

    add users pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,3 +22,7 @@ Route::post('signup','Auth\AuthController@postRegister')->name('signup.post');
 Route::get('login','Auth\AuthController@getLogin')->name('login.get');
 Route::post('login','Auth\AuthController@postLogin')->name('login.post');
 Route::get('logout','Auth\AuthController@getLogout')->name('logout.get');
+//ユーザー一覧・詳細
+Route::group(['middleware' => 'auth'],function(){
+    Route::resource('users','UserController',['only' => ['index','show']]);
+});
